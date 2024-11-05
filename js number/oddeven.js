f6bba27815a72fac2663b4872393f091f5a446a6
Yufei-Chang <yufei.chang@hotmail.com>
2024-11-05 17:57:14 +0100

    feat: something

--- js number/oddeven.js
+++ js number/oddeven.js
@@ -1,23 +1,25 @@
+// Chiediamo all'utente di scegliere
 const choice = prompt("Scegli tra pari o dispari").toLowerCase();
 
 let userNum = parseInt(prompt("Scegli un numero da 1 a 5"));
 console.log(`Il tuo numero è: ${userNum}`);
 
+// Input automatico del computer
 const cpuNum = randomNum();
 function randomNum() {
     return Math.floor(Math.random() * 5) + 1;
 }
 console.log(`Il numero del computer è: ${cpuNum}`);
 
-function oddeven(number) {
-    return number % 2 === 0 ? 'pari' : 'dispari';
-}
-
 const result = userNum + cpuNum;
 console.log(`La somma dei vostri numeri è: ${result}`);
 
 const finalResult = oddeven(result);
 console.log(`La somma è ${finalResult}.`);
+
+function oddeven(number) {
+    return number % 2 === 0 ? 'pari' : 'dispari';
+}
 
 if (finalResult === choice) {
     console.log("You win! Humanity safe!");
--- js word/palindroma.js
+++ js word/palindroma.js
@@ -1,10 +1,13 @@
+// Chiediamo la parola all'utente
 const userWord = prompt("Inserisci una parola")
 
+// Controlliamo se è palindroma
 function isPalindroma(userWord) {
     const reverseWord = userWord.split('').reverse().join('');
     return userWord === reverseWord;
 }
 
+// Mostriamo il risultato del controllo
 if (isPalindroma(userWord)) {
     console.log(`La parola "${userWord}" è palindroma!`);
 } else {
