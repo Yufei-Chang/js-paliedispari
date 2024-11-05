3e6eb3f5357b09eeb9f52c43513e812b050a8ab5
Yufei-Chang <yufei.chang@hotmail.com>
2024-11-05 17:52:19 +0100

    feat: adjusted

--- js number/oddeven.js
+++ js number/oddeven.js
@@ -1,28 +1,26 @@
+const choice = prompt("Scegli tra pari o dispari").toLowerCase();
+
+let userNum = parseInt(prompt("Scegli un numero da 1 a 5"));
+console.log(`Il tuo numero è: ${userNum}`);
+
+const cpuNum = randomNum();
 function randomNum() {
     return Math.floor(Math.random() * 5) + 1;
 }
+console.log(`Il numero del computer è: ${cpuNum}`);
 
 function oddeven(number) {
     return number % 2 === 0 ? 'pari' : 'dispari';
 }
 
-const choice = prompt("Scegli tra pari o dispari").toLowerCase();
+const result = userNum + cpuNum;
+console.log(`La somma dei vostri numeri è: ${result}`);
 
-let userNum = parseInt(prompt("Scegli un numero da 1 a 5"));
+const finalResult = oddeven(result);
+console.log(`La somma è ${finalResult}.`);
 
-if (userNum < 1 || userNum > 5 || isNaN(userNum)) {
-    const cpuNum = randomNum();
-    console.log(`Il numero del computer è: ${numeroComputer}`);
-
-    const result = userNum + cpuNum;
-    console.log(`La somma dei numeri è: ${somma}`);
-
-    const finalResult = oddeven(result);
-    console.log(`La somma è ${finalResult}.`);
-
-    if (finalResult === choice) {
-        console.log("You win! Humanity safe!");
+if (finalResult === choice) {
+    console.log("You win! Humanity safe!");
     } else {
-        console.log("CPU wins! Humanity doomed!");
+    console.log("CPU wins! Humanity doomed!");
     }
-}
